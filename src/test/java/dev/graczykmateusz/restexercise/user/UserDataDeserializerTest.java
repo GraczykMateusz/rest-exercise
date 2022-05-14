911bea1b81d775b3fee204d3ffa6053cd9df0b90
GraczykMateusz <dev.graczykmateusz@gmail.com>
2022-05-14 19:29:15 +0200

    refactor

--- src/test/java/dev/graczykmateusz/restexercise/user/UserDataDeserializerTest.java
+++ src/test/java/dev/graczykmateusz/restexercise/user/UserDataDeserializerTest.java
@@ -25,40 +25,30 @@ class UserDataDeserializerTest {
 
     @Test
     void shouldDeserializeJsonUser() throws IOException, JSONException {
-        //given
         String userNonZeroFollowersResponse = GithubClientResponseProvider.getUserNonZeroFollowersResponse();
         ObjectMapper objectMapper = new ObjectMapper();
         JsonParser jp = objectMapper.getFactory().createParser(userNonZeroFollowersResponse);
 
-        //when
         UserData actual = underTest.deserialize(jp, null);
 
-        //then
-        Long id = 43554417L;
-        String login = "GraczykMateusz";
-        String name = "Mateusz Graczyk";
-        String type = "User";
-        String avatarUrl = "https://avatars.githubusercontent.com/u/43554417?v=4";
-        Instant createdAt = Instant.parse("2018-09-24T20:41:09Z");
-        double calculations = 38.0;
-
         UserData expected = new UserData(
-                id, login, name,
-                type, avatarUrl, createdAt,
-                calculations);
+                43554417L,
+                "GraczykMateusz",
+                "Mateusz Graczyk",
+                "User",
+                "https://avatars.githubusercontent.com/u/43554417?v=4",
+                Instant.parse("2018-09-24T20:41:09Z"),
+                38.0);
 
         assertThat(actual).isEqualTo(expected);
     }
 
     @Test
     void shouldThrowIllegalCalculationsExceptionWhenFollowersCountEqualsZero() throws IOException, JSONException {
-        //given
         String userZeroFollowersResponse = GithubClientResponseProvider.getUserZeroFollowersResponse();
         ObjectMapper objectMapper = new ObjectMapper();
         JsonParser jp = objectMapper.getFactory().createParser(userZeroFollowersResponse);
 
-        //when
-        //then
         assertThatThrownBy(() -> underTest.deserialize(jp, null))
                 .isInstanceOf(ZeroFollowersException.class)
                 .hasMessage("Calculation cannot be performed because the number of followers is zero!");
