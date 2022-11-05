731e96f86dd7d8e589b4b66e7414bcab6a01757a
Mateusz Graczyk <dev.graczykmateusz@gmail.com>
2022-11-05 22:00:50 +0100

    fix security

--- src/main/java/dev/graczykmateusz/restexercise/config/SecurityConfig.java
+++ src/main/java/dev/graczykmateusz/restexercise/config/SecurityConfig.java
@@ -23,7 +23,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http.headers().frameOptions().sameOrigin();
+
+        http.csrf().disable()
+                .authorizeRequests()
                 .antMatchers("/actuator/**").permitAll()
                 .antMatchers("/**").hasRole("ADMIN")
                 .and()
